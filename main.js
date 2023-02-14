505a9133801c93ca41a3879b62f91cf261eab944
Dan <84781982+danisacoder@users.noreply.github.com>
2023-02-13 19:42:51 -0500

    Parallax demo complete

--- main.js
+++ main.js
@@ -2,14 +2,35 @@ const wrapperDiv = document.querySelector('.wrapper')
 
 wrapperDiv.addEventListener('scroll', function(e) {
     
-    console.log(wrapperDiv.pageYOffset)
+    const mountains = document.querySelector('.mountains')
 
-    const target = document.querySelector('.far-grass')
+    const topGrass = document.querySelector('.top-grass')
 
-    console.log(target.style)
+    const midGrass = document.querySelector('.mid-grass')
 
-    target.style.transform = 'translate3d(0px, 50px, 0px)'
+    const bottomGrass = document.querySelector('.bottom-grass')
+
+    var scrolledAmnt = wrapperDiv.scrollTop
+
+    let mountainScroll = scrolledAmnt * .8
+
+    let topGrassScroll = scrolledAmnt * .7
+
+    let midGrassScroll = scrolledAmnt * .6
+
+    let bottomGrassScroll = scrolledAmnt * .5
+
+
+    mountains.style.transform = `translate3d(0px, ${mountainScroll}px, 0px)`
+
+
+    topGrass.style.transform = `translate3d(0px, ${topGrassScroll}px, 0px)`
+
+    midGrass.style.transform = `translate3d(0px, ${midGrassScroll}px, 0px)`
+
+    bottomGrass.style.transform = `translate3d(0px, ${bottomGrassScroll}px, 0px)`
 
 })
  
+
  
