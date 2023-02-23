570366064c47ab1c432839edf4841822b248395a
Dan <84781982+danisacoder@users.noreply.github.com>
2023-02-23 17:19:05 -0500

    Moving and cleaning up images, added Dylan button

--- main.js
+++ main.js
@@ -1,47 +1,51 @@
 const wrapperDiv = document.querySelector('.wrapper')
 
-wrapperDiv.addEventListener('scroll', function(e) {
+if (wrapperDiv) {
     
-// Background paper scroll speed controls
-
-    const mountains1 = document.querySelector('.mountain-layer-1')
-    const mountains2 = document.querySelector('.mountain-layer-2')
-    const mountains3 = document.querySelector('.mountain-layer-3')
-    const mountains4 = document.querySelector('.mountain-layer-4')
-    const mountains5 = document.querySelector('.mountain-layer-5')
-    const mountains6 = document.querySelector('.mountain-layer-6')
-
+    wrapperDiv.addEventListener('scroll', function(e) {
     
+    // Background paper scroll speed controls
 
-    var scrolledAmnt = wrapperDiv.scrollTop
+        const mountains1 = document.querySelector('.mountain-layer-1')
+        const mountains2 = document.querySelector('.mountain-layer-2')
+        const mountains3 = document.querySelector('.mountain-layer-3')
+        const mountains4 = document.querySelector('.mountain-layer-4')
+        const mountains5 = document.querySelector('.mountain-layer-5')
+        const mountains6 = document.querySelector('.mountain-layer-6')
 
-    let mountains1scroll = scrolledAmnt * .8
+        
 
-    let mountains2scroll = scrolledAmnt * .7
+        var scrolledAmnt = wrapperDiv.scrollTop
 
-    let mountains3scroll = scrolledAmnt * .6
+        let mountains1scroll = scrolledAmnt * .8
 
-    let mountains4scroll = scrolledAmnt * .5
+        let mountains2scroll = scrolledAmnt * .7
 
-    let mountains5scroll = scrolledAmnt * .4
+        let mountains3scroll = scrolledAmnt * .6
 
-    let mountains6scroll = scrolledAmnt * .3
+        let mountains4scroll = scrolledAmnt * .5
+
+        let mountains5scroll = scrolledAmnt * .4
+
+        let mountains6scroll = scrolledAmnt * .3
 
 
-    mountains1.style.transform = `translate3d(0px, ${mountains1scroll}px, 0px)`
+        mountains1.style.transform = `translate3d(0px, ${mountains1scroll}px, 0px)`
 
 
-    mountains2.style.transform = `translate3d(0px, ${mountains2scroll}px, 0px)`
+        mountains2.style.transform = `translate3d(0px, ${mountains2scroll}px, 0px)`
 
-    mountains3.style.transform = `translate3d(0px, ${mountains3scroll}px, 0px)`
+        mountains3.style.transform = `translate3d(0px, ${mountains3scroll}px, 0px)`
 
-    mountains4.style.transform = `translate3d(0px, ${mountains4scroll}px, 0px)`
+        mountains4.style.transform = `translate3d(0px, ${mountains4scroll}px, 0px)`
 
-    mountains5.style.transform = `translate3d(0px, ${mountains5scroll}px, 0px)`
+        mountains5.style.transform = `translate3d(0px, ${mountains5scroll}px, 0px)`
 
-    mountains6.style.transform = `translate3d(0px, ${mountains6scroll}px, 0px)`
+        mountains6.style.transform = `translate3d(0px, ${mountains6scroll}px, 0px)`
 
-})
+    })
+
+}
 
 // changing the style of the current link in menu so you know where you are
 
@@ -62,6 +66,8 @@ var currentFileName = location.href.split("/").slice(-1).join('').split('.').shi
 console.log(currentFileName)
 
 const currentPage = window.location.pathname;
+
+// console.log(currentPage)
 
 for (let entry of menuLinkListArray) { 
 
